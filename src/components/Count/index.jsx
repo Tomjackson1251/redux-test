41a535f0c0e7adb60d023ce5b1c87bbb8c61e778
Tom <hty1251817231@gmail.com>
2021-03-11 21:18:01 +0800

    dsa

--- src/components/Count/index.jsx
+++ src/components/Count/index.jsx
@@ -1,50 +1,38 @@
 import React, { Component } from 'react'
+import store from '../../redux/store'
 
 export default class Count extends Component {
-  state = {
-    count: 0,
-  }
+  
 
   increment = () => {
     const { value } = this.selectNumber
-    const { count } = this.state
-    this.setState({
-      count: count + value * 1,
-    })
+    store.dispatch({ type: 'increment', data: value * 1 })
   }
 
   decrement = () => {
     const { value } = this.selectNumber
-    const { count } = this.state
-    this.setState({
-      count: count - value * 1,
-    })
+    store.dispatch({ type: 'decrement', data: value * 1 })
   }
 
   incrementIfOdd = () => {
     const { value } = this.selectNumber
-    const { count } = this.state
+    const count = store.getState()
     if (count % 2 !== 0) {
-      this.setState({
-        count: count + value * 1,
-      })
+      store.dispatch({ type: 'increment', data: value * 1 })
     }
   }
 
   incrementAsync = () => {
     const { value } = this.selectNumber
-    const { count } = this.state
     setTimeout(() => {
-      this.setState({
-        count: count + value * 1,
-      })
+      store.dispatch({ type: 'increment', data: value * 1 })
     }, 500)
   }
 
   render() {
     return (
       <div>
-        <h1>当前求和为：{this.state.count}</h1>
+        <h1>当前求和为：{store.getState()}</h1>
         <select ref={(c) => (this.selectNumber = c)}>
           <option value="1">1</option>
           <option value="2">2</option>
---
+++ src/redux/store.js
@@ -0,0 +1,4 @@
+import { createStore } from 'redux'
+import countreducer from './count_reducer'
+
+export default createStore(countreducer)
